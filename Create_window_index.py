0e46aa11ca3b66e06e8b2e612d0a61005859b061
Emad <emad.arezoomandd@gmail.com>
2024-04-28 22:57:20 +0330

    windower added

--- Create_window_index.py
+++ Create_window_index.py
@@ -10,3 +10,10 @@ def create_window_indexes (len_data,len_window):
         s +=1 ; e += 1
     return np.array(indexes)
 
+def apply_windower_to_series_data(data, labels,windows_indexes):
+    dataset = []
+    for i in range(len(windows_indexes)):
+        chunk_data = data.iloc[windows_indexes[i]]
+        associated_label = labels[[windows_indexes[i][-1]]]
+        dataset.append((chunk_data,associated_label))
+    return dataset
